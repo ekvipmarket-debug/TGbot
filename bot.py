ce25481d55f9a2f3af9875c56bd4b672b601b017
ekvipmarket-debug <ekvipmarket@gmail.com>
2025-09-28 13:30:14 +0300

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,13 @@
+from telegram import Bot
+
+TOKEN = AAH__ie40UxlVUOMw-dXP1D5tewFGBAp2tY
+CHANNEL_ID = https://t.me/tatyshcho
+
+bot = Bot(token=TOKEN)
+
+def post_news():
+    news_text = "Привет! Это автоматическая новость от бота."
+    bot.send_message(chat_id=CHANNEL_ID, text=news_text)
+
+if __name__ == "__main__":
+    post_news()
